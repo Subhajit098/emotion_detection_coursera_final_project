62802e6a3791c9bf8f944c4495ba98c5898e7cb9
androidjit07 <sd.20u10020@btech.nitdgp.ac.in>
2024-08-02 13:13:33 +0530

    implement try and catch

--- emotionDetection/emotion_detection.py
+++ emotionDetection/emotion_detection.py
@@ -5,11 +5,14 @@ def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(URL, json = input_json, headers=header)
-    formated_response = json.loads(response.text)
 
-    if response.status_code == 200:
-        # return formated_response
+    try : 
+        response = requests.post(URL, json = input_json, headers=header)
+        formated_response = json.loads(response.text)
+
+        response.raise_for_status() 
+        # raise an HTTP error for bad responses(4xx and 5xx)
+
         extracted_emotion=formated_response['emotionPredictions'][0]['emotion']
         # return extracted_emotion
 
@@ -27,12 +30,24 @@ def emotion_detector(text_to_analyze):
 
         return extracted_emotion
 
-    elif response.status_code == 400:
-        formated_response = {
-                            'anger': None,
-                            'disgust': None, 
-                            'fear': None, 
-                            'joy': None, 
-                            'sadness': None, 
-                            'dominant_emotion': None}
-        return formated_response
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request error occurred: {req_err}")
+    except KeyError as key_err:
+        print(f"Key error: {key_err}, response content: {response.text}")
+    except json.JSONDecodeError as json_err:
+        print(f"JSON decode error: {json_err}, response content: {response.text}")
+    
+    # Return a default structure in case of error
+    return {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+    }
+
+
+    
